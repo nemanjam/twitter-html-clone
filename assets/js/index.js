1dc2e4222fd53ab4be314c2c2215851b46e421ac
nemanjam <nemanja.mitic.elfak@hotmail.com>
2020-05-06 21:17:27 +0200

    left header

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+// import "../styles/styles.scss";
+// document.getElementById("app").innerHTML = '<h1>Hello Vanilla1!</h1>';
+import bird from "../icons/bird.svg";
+
+// document.getElementById("bird-svg").innerHTML = bird;
+
+console.log("loaded");
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import "./styles.scss";
-
-// document.getElementById("app").innerHTML = '<h1>Hello Vanilla1!</h1>';
-console.log("js linked");
